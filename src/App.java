b010e1c8dd830ce48d7b26e1fc38cff726b8b3f6
RemsNoel <remy.noel@epsi.fr>
2019-10-08 16:16:44 +0200

    Create Classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.InputMismatchException;
+
 public class App {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+
 
     }
 }
---
+++ src/bo/typeCompte/ComptePayant.java
@@ -0,0 +1,4 @@
+package bo.typeCompte;
+
+public class ComptePayant {
+}
